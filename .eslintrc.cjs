d69f760f7db59b0b94af910eb5bedee58629e185
Nour El Din Shady <rhasoldy@gmail.com>
2024-01-19 00:42:19 +0200

    chore: added postcss, eslint, prettier... etc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,25 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
     "plugin:storybook/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "prettier.config.cjs", "postcss.config.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
   plugins: ["react-refresh"],
   rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    indent: ["warn", 2],
+    "linebreak-style": ["warn", "unix"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
---
+++ prettier.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  useTabs: false,
+  endOfLine: "auto",
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^components/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+};
