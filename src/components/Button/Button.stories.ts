01a5fa6bc678f8ca44eae9e81d16b460401bc545
Nour El Din Shady <rhasoldy@gmail.com>
2024-01-19 13:17:53 +0200

    fix: removed deleted props

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -14,9 +14,6 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -25,7 +22,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
     label: "Button",
   },
 };
@@ -38,14 +34,12 @@ export const Secondary: Story = {
 
 export const Large: Story = {
   args: {
-    size: "large",
     label: "Button",
   },
 };
 
 export const Small: Story = {
   args: {
-    size: "small",
     label: "Button",
   },
 };
