c8d5c603065df828c1c110850836a2ab960decbf
Nour El Din Shady <rhasoldy@gmail.com>
2024-01-19 12:01:21 +0200

    chore: added more plugins for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,71 @@
 module.exports = {
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "prettier.config.cjs",
+    "postcss.config.cjs",
+    "!.storybook",
+  ],
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "prettier.config.cjs", "postcss.config.cjs", "!.storybook"],
+  plugins: [
+    "react",
+    "react-hooks",
+    "storybook",
+    "jsx-a11y",
+    "react-refresh",
+    "import",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:import/typescript",
+      ],
+      plugins: ["@typescript-eslint/eslint-plugin"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/no-floating-promises": [
+          "error",
+          { ignoreVoid: true },
+        ],
+      },
+    },
+  ],
+  settings: {
+    ecmaVersion: "latest",
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: true,
+      typescript: true,
+    },
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    ecmaFeatures: { jsx: true }
+    ecmaFeatures: { jsx: true },
   },
-  plugins: ["react-refresh"],
   rules: {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
@@ -27,6 +77,29 @@ module.exports = {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "$/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        groups: [
+          ["builtin", "external"],
+          ["internal"],
+          ["parent", "sibling", "index"],
+          "unknown",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
   },
 };
