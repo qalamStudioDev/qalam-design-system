e6ad4dc844309b0db2540c880a4674199174c3b9
Nour El Din Shady <rhasoldy@gmail.com>
2024-01-27 23:24:42 +0200

    feat(build): added dts and other configs for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteTsConfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: "src/main.ts",
